74a307169a89351d41f17b555ec081a2a4743267
psy <thdud4548@naver.com>
2020-06-24 07:13:32 +0900

    1

---
+++ mash/calct.py
@@ -0,0 +1,16 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# In[1]:
+
+
+import clas_calc
+calc=clas_calc.Calc()
+calc.div(4,2)
+
+
+# In[ ]:
+
+
+
+
---
+++ mash/clas_calc.py
@@ -0,0 +1,11 @@
+#!/usr/bin/env python
+#coding=utf8
+class Calc:
+    def sum(self,a,b):
+        print("{0}+{1}={2} 입니다.".format(a,b,a+b))
+    def sub(self,a,b):
+        print("{0}+{1}={2} 입니다.".format(a,b,a-b))
+    def multi(self,a,b):
+        print("{0}+{1}={2} 입니다.".format(a,b,a*b))
+    def div(self,a,b):
+        print("{0}+{1}={2} 입니다.".format(a,b,a/b))
